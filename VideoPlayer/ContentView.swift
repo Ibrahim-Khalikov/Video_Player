7546a328d62fb323e63e6ed98d73d0555223a3c1
Ibrahim Khalikov <starlink050@gmail.com>
2021-08-09 21:11:00 +0500

    Initial Commit

---
+++ VideoPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoPlayer
+//
+//  Created by cloud_vfx on 09/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by cloud_vfx on 09/08/21.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
